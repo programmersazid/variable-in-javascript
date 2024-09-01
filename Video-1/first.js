769965203dbc0d1079e00b61f45459e349d7ab74
modabbir <modabbir276@gmail.com>
2024-09-02 01:10:12 +0600

    variable work done

---
+++ Video-1/first.js
@@ -0,0 +1,20 @@
+// Normal Variable
+console.log(33);
+var price;
+price = 20;
+console.log(price);
+
+// String type variable
+var name = "Modabbir Hasan Sazid";
+console.log(name);
+//boolean typer variable
+var passed = false;
+console.log(passed);
+// ki typer variable
+console.log(typeof passed);
+console.log(typeof price);
+console.log(typeof name);
+
+var current_home_address ;  //snake case
+var myCurrentHomeAddress; //camel case
+var MyCurrentHomeAddress;//pascal case
